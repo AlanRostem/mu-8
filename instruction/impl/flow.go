52b0a0839b01050ed06b17b2c9fb38d461640c6f
AlanRostem <arostem@gmail.com>
2026-04-06 20:47:19 +0200

    Fix debug logs

--- instruction/impl/flow.go
+++ instruction/impl/flow.go
@@ -16,30 +16,30 @@ func Call(args []mu8.DByte, sys *system.System) {
 
 func Jp(args []mu8.DByte, sys *system.System) {
 	nnn := args[0]
+	pcDebugf(sys.Registers.ProgramCounter, "JP 0x%03X", nnn)
 	// HACK since the PC is incremented always, we offset it by -2 so it lands on
 	// the desired address later
 	sys.Registers.ProgramCounter = nnn - 2
-	pcDebugf(sys.Registers.ProgramCounter, "JP 0x%03X", nnn)
 }
 
 // 3xkk
 func SeVxByte(args []mu8.DByte, sys *system.System) {
 	x := args[0]
 	kk := args[1]
+	pcDebugf(sys.Registers.ProgramCounter, "SE V%01X, %d", x, kk)
 	vx := sys.Registers.GeneralPurpose[x]
 	if mu8.DByte(vx) == kk {
 		sys.Registers.ProgramCounter += 2
 	}
-	pcDebugf(sys.Registers.ProgramCounter, "SE V%01X, %d", x, kk)
 }
 
 // 3xkk
 func SneVxByte(args []mu8.DByte, sys *system.System) {
 	x := args[0]
 	kk := args[1]
+	pcDebugf(sys.Registers.ProgramCounter, "SNE V%01X, %d", x, kk)
 	vx := sys.Registers.GeneralPurpose[x]
 	if mu8.DByte(vx) != kk {
 		sys.Registers.ProgramCounter += 2
 	}
-	pcDebugf(sys.Registers.ProgramCounter, "SNE V%01X, %d", x, kk)
 }
